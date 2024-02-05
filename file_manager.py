be498a1132175fb514253812e74113ca7963ec10
Turki Al-Suhaibani <112579033+TurkiOS@users.noreply.github.com>
2024-02-05 13:56:24 +0300

    file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,232 +1,154 @@
+import customtkinter as ctk
+from tkinter import messagebox
 import os
 import shutil
 from pathlib import Path
-from colorama import Fore, Back, Style
-
-## Downloads Directory
-print(Fore.LIGHTMAGENTA_EX + """
- 
-
-    _______ __                                                   
-   / ____(_) /__     ____ ___  ____ _____  ____ _____ ____  _____
-  / /_  / / / _ \   / __ `__ \/ __ `/ __ \/ __ `/ __ `/ _ \/ ___/
- / __/ / / /  __/  / / / / / / /_/ / / / / /_/ / /_/ /  __/ /    
-/_/   /_/_/\___/  /_/ /_/ /_/\__,_/_/ /_/\__,_/\__, /\___/_/     
-                                              /____/          V 1.0  
-
-    
-Find me on X :_TurkI0S
-
-""")
-
-print("""1- Orgnize Downloads Folder
-2- Coming Soon""")
-User = input()
-if int(User) != 1:
-    print("Other Futures Coming Soon ^_^")
-else:
-    print(Fore.LIGHTGREEN_EX)
-## Creating Documents Folder (Documents)
-    current_dir = Path.home() / 'Downloads'
-    os.chdir(current_dir)
-
-    for filename in os.listdir(current_dir):
-
-        if filename.lower().endswith((".pdf",".doc",".docx",".pptx",".ppt",".xls",".xlsx",".txt")):
-        ## Creating folder "Documents" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents')
-            shutil.copy2(filename, "Documents")
-            os.remove(filename)
-    print("Documents Folder Created")
-
-    ## Creating other folders (PDF)
-
-    documents_dir =  Path.home() / 'Downloads' / 'Documents'
-    os.chdir(documents_dir)
-
-    for filename in os.listdir(documents_dir):
-        if filename.lower().endswith((".pdf")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents' / 'PDF'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents' / 'PDF')
-            shutil.copy2(filename, "PDF")
-            os.remove(filename)
-            print(filename, "--> Done (PDF)")
-    print("PDF Folder complete")
-
-## Creating other folders (Text)
-
-    for filename in os.listdir(documents_dir):
-        if filename.lower().endswith((".txt")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents' / 'Text'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents' / 'Text')
-            shutil.copy2(filename, "Text")
-            os.remove(filename)
-            print(filename, "--> Done (Text)")
-    print("Text Folder complete")
+from tkinter import filedialog
 
 
-## Creating other folders (Word)
+# Basic parameters and initializations
+appWidth, appHeight = 700, 400
 
-    for filename in os.listdir(documents_dir):
-        if filename.lower().endswith((".doc",".docx")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents' / 'Word'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents' / 'Word')
-            shutil.copy2(filename, "Word")
-            os.remove(filename)
-            print(filename, "--> Done (Word)")
-    print("Word Folder complete")
+# Supported modes: Light, Dark, System
+ctk.set_appearance_mode("Dark")
 
-    ## Creating other folders (PowerPoint)
 
-    for filename in os.listdir(documents_dir):
-        if filename.lower().endswith((".ppt",".pptx")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents' / 'PowerPoint'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents' / 'PowerPoint')
-            shutil.copy2(filename, "PowerPoint")
-            os.remove(filename)
-            print(filename, "--> Done (PowerPoint)")
-    print("PowerPoint Folder complete")
+# Supported themes: green, dark-blue, blue
+ctk.set_default_color_theme("dark-blue")
 
-    ## Creating other folders (Excel)
+# App Class
+class App(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    for filename in os.listdir(documents_dir):
-        if filename.lower().endswith((".xls",".xlsx")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Documents' / 'Excel'):
-                os.mkdir(Path.home() / 'Downloads' / 'Documents' / 'Excel')
-            shutil.copy2(filename, "Excel")
-            os.remove(filename)
-            print(filename, "--> Done (Excel)")
-    print("Excel Folder complete")
+        # Remove title bar
+        self.overrideredirect(True)
 
-    ## Creating Images Folder (Images)
+        screen_width = self.winfo_screenwidth() + 215
+        screen_height = self.winfo_screenheight() + 50
 
-    os.chdir(current_dir)
+        x = int((screen_width / 2) - (appWidth / 2))
+        y = int((screen_height / 2) - (appHeight / 2))
 
-    for filename in os.listdir(current_dir):
+        self.title("File Manager")
+        self.geometry(f"{appWidth}x{appHeight}+{x}+{y}")
 
-        if filename.lower().endswith((".jpeg",".jpg",".gif",".tiff",".raw","png")):
-        ## Creating folder "Images" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Images'):
-                os.mkdir(Path.home() / 'Downloads' / 'Images')
-            shutil.copy2(filename, "Images")
-            os.remove(filename)
-    print("Images Folder Created")
+        # Set the transparency level of the window to 95%
+        self.attributes("-alpha", 0.95)
 
-    ## Creating Videos Folder (Videos)
+        # File Manager Label
+        self.FMLabel = ctk.CTkLabel(self, text="File Manager",text_color="#47CA86", font=("Ubuntu Medium",15))
+        self.FMLabel.grid(row=0, column=0, columnspan=3, padx=20, pady=10, sticky="ew")
 
-    for filename in os.listdir(current_dir):
+        # Manage Label
+        self.manageLabel = ctk.CTkLabel(self, text="Manage Your Directories",text_color="#bff8da", font=("Ubuntu",42))
+        self.manageLabel.grid(row=1, column=0, columnspan=3, padx=20, pady=1, sticky="ew")
 
-        if filename.lower().endswith((".mp4",".mov",".avi",".wmv",".avchd",".webm","flv")):
-        ## Creating folder "Videos" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Videos'):
-                os.mkdir(Path.home() / 'Downloads' / 'Videos')
-            shutil.copy2(filename, "Videos")
-            os.remove(filename)
-    print("Videos Folder Created")
+        # Author Label
+        self.FMLabel = ctk.CTkLabel(self, text="By TurkIOS",text_color="#bff8da", font=("Ubuntu",20))
+        self.FMLabel.grid(row=2, column=0, columnspan=3, padx=20, pady=1, sticky="ew")
 
-    ## Creating Applications Folder (Applications)
+        # Select directory Button
+        self.desktop_directory_Button = ctk.CTkButton(self, width=120,height=40,hover_color="#72A088",text_color="#2E4639",fg_color="#b4f4d2", text="Select Directory",font=("Ubuntu",20), command=self.manage_directory)
+        self.desktop_directory_Button.grid(row=3, column=0, columnspan=3, padx=250, pady=60, sticky="ew")
 
-    for filename in os.listdir(current_dir):
+        # Quit Button
+        self.quit_Button = ctk.CTkButton(self, text="Close", hover_color="#72A088", text_color="#2E4639", fg_color="#b4f4d2" ,command=self.quit)
+        self.quit_Button.grid(row=4, column=1, padx=300, pady=70, sticky="ew")
+        # Configure row and column to center the label and button
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=0)
+        self.columnconfigure(1, weight=1)
+        # Configure row and column to center the label and button
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=0)
+        self.columnconfigure(1, weight=1)
 
-        if filename.lower().endswith((".exe",".bat","rmskin")):
-        ## Creating folder "Applications" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Applications'):
-                os.mkdir(Path.home() / 'Downloads' / 'Applications')
-            shutil.copy2(filename, "Applications")
-            os.remove(filename)
-    print("Applications Folder Created")
-    print(Fore.LIGHTYELLOW_EX)
-    print("Processing.... it may take a few minutes")
-    print(Fore.LIGHTGREEN_EX)
-    ## Creating Archive Folder (Archive)
+    def manage_directory(self):
+        root = ctk.CTk()
+        root.withdraw()
 
-    for filename in os.listdir(current_dir):
+        directory_path = filedialog.askdirectory(title="Select Directory")
+        if not directory_path:
+            return
 
-        if filename.lower().endswith((".tar",".rar",".zip",".genozip",".sfark",".7z",".ace",".apk")):
-        ## Creating folder "Archive" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Archive'):
-                os.mkdir(Path.home() / 'Downloads' / 'Archive')
-            shutil.copy2(filename, "Archive")
-            os.remove(filename)
-    print("Archive Folder Created")
+        current_dir = Path(directory_path)
+        os.chdir(current_dir)
 
-    ## Creating Codes Folder (Codes)
+        # Create directories
+        documents_dir = current_dir / 'Documents'
+        codes_dir = current_dir / 'Codes'
+        archive_dir = current_dir / 'Archive'
+        applications_dir = current_dir / 'Applications'
+        images_dir = current_dir / 'Images'
+        videos_dir = current_dir / 'Videos'
 
-    for filename in os.listdir(current_dir):
+        for directory in [documents_dir, codes_dir, archive_dir, applications_dir, images_dir, videos_dir]:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
 
-        if filename.lower().endswith((".java",".c",".c++",".cpp",".html",".py")):
-        ## Creating folder "Codes" to hold files
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes'):
-                os.mkdir(Path.home() / 'Downloads' / 'Codes')
-            shutil.copy2(filename, "Codes")
-            os.remove(filename)
-    print("Codes Folder Created")
+        # Move supported documents to the Documents folder
+        supported_documents = (".pdf", ".doc", ".docx", ".pptx", ".ppt", ".xls", ".xlsx", ".txt")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_documents):
+                file_extension = os.path.splitext(filename)[1].lower()
 
-    ## Creating other folders (Python)
+                if file_extension == '.pdf':
+                    subdir = Path('PDFs')
+                elif file_extension in ('.doc', '.docx'):
+                    subdir = Path('Word')
+                elif file_extension in ('.ppt', '.pptx'):
+                    subdir = Path('PowerPoint')
+                elif file_extension in ('.xls', '.xlsx'):
+                    subdir = Path('Excel')
+                elif file_extension == '.txt':
+                    subdir = Path('Text')
+                else:
+                    continue  # Skip moving if the file extension is not supported
 
-    Codes_dir =  Path.home() / 'Downloads' / 'Codes'
-    os.chdir(Codes_dir)
+                # Create the subdirectory inside the Documents folder if it doesn't exist
+                subdirectory_path = documents_dir / subdir
+                if not os.path.exists(subdirectory_path):
+                    os.makedirs(subdirectory_path)
 
-    for filename in os.listdir(Codes_dir):
-        if filename.lower().endswith((".py")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes' / 'Python'):
-                os.mkdir(Path.home() / 'Downloads'/ 'Codes' / 'Python')
-            shutil.copy2(filename, "Python")
-            os.remove(filename)
-            print(filename, "--> Done (Python)")
-    print("Python Folder complete")
+                # Move the file to the subdirectory
+                shutil.move(os.path.join(current_dir, filename), subdirectory_path)
 
-    ## Creating other folders (WEB)
+        # Move code files to the Codes folder
+        supported_code_files = (".py", ".java", ".cpp", ".c", ".html", ".css", ".js")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_code_files):
+                shutil.move(os.path.join(current_dir, filename), codes_dir)
 
-    for filename in os.listdir(Codes_dir):
-        if filename.lower().endswith((".html")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes' / 'WEB'):
-                os.mkdir(Path.home() / 'Downloads' / 'Codes' / 'WEB')
-            shutil.copy2(filename, "WEB")
-            os.remove(filename)
-            print(filename, "--> Done (WEB)")
-    print("WEB Folder complete")
+        # Move compressed files to the Archive folder
+        supported_archive_files = (".zip", ".rar", ".tar", ".gz")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_archive_files):
+                shutil.move(os.path.join(current_dir, filename), archive_dir)
 
-    ## Creating other folders (C++)
+        # Move executable files to the Applications folder
+        supported_application_files = (".exe", ".dmg", ".pkg", "rmskin")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_application_files):
+                shutil.move(os.path.join(current_dir, filename), applications_dir)
 
-    for filename in os.listdir(Codes_dir):
-        if filename.lower().endswith((".c++","cpp")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes' / 'C++'):
-                os.mkdir(Path.home() / 'Downloads' / 'Codes' / 'C++')
-            shutil.copy2(filename, "C++")
-            os.remove(filename)
-            print(filename, "--> Done (C++)")
-    print("C++ Folder complete")
+        # Move image files to the Images folder
+        supported_image_files = (".jpg", ".jpeg", ".png", ".gif")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_image_files):
+                shutil.move(os.path.join(current_dir, filename), images_dir)
 
-    ## Creating other folders (C)
+        # Move video files to the Videos folder
+        supported_video_files = (".mp4", ".avi", ".mov", ".wmv")
+        for filename in os.listdir(current_dir):
+            if filename.lower().endswith(supported_video_files):
+                shutil.move(os.path.join(current_dir, filename), videos_dir)
 
-    for filename in os.listdir(Codes_dir):
-        if filename.lower().endswith((".c")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes' / 'C'):
-                os.mkdir(Path.home() / 'Downloads' / 'Codes' / 'C')
-            shutil.copy2(filename, "C")
-            os.remove(filename)
-            print(filename, "--> Done (C)")
-    print("C Folder complete")
+        # Show "Done" message box
+        messagebox.showinfo("File Manager", "Operation completed.")
 
-    ## Creating other folders (Java)
 
-    for filename in os.listdir(Codes_dir):
-        if filename.lower().endswith((".java")):
-            if not os.path.exists(Path.home() / 'Downloads' / 'Codes' / 'Java'):
-                os.mkdir(Path.home() / 'Downloads' / 'Codes' / 'Java')
-            shutil.copy2(filename, "Java")
-            os.remove(filename)
-            print(filename, "--> Done (Java)")
-    print("Java Folder complete")
-    print(Fore.LIGHTMAGENTA_EX)
-    print("""
-Exiting Program...
-
-Done good to go 
-          
-          
-          """)
-print(Style.RESET_ALL)
-
+# Create and run the app
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
